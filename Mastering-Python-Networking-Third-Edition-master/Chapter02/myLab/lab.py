8eaaa7b50ffac6a11c5b936d15a38c1ab6e2f869
sjtiantang <sjtiantang@gmail.com>
2020-12-08 15:06:26 -0800

    add myLab 12/8/2020

--- Mastering-Python-Networking-Third-Edition-master/Chapter02/myLab/lab.py
+++ Mastering-Python-Networking-Third-Edition-master/Chapter02/myLab/lab.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python
-
 from nornir import InitNornir
 from nornir_netmiko.tasks import netmiko_send_command
 from nornir_utils.plugins.functions import print_result
 
-nr = InitNornir()
+nr = InitNornir(config_file="config.yml")
 
 result = nr.run(
     task=netmiko_send_command,
-    command_string="show arp"
+    command_string='show ip int bri'
 )
 
 print_result(result)
